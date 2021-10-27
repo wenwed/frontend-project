467478b041ff3996f7368f1dc9d1f029ceb3a12c
wenwd <1583925120@qq.com>
2021-10-27 17:09:14 +0800

    update

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const ROWSIZE = 4;  // 列数
 const COLUMNSIZE = 4;   // 行数
 let totalOperate = 0;   // 操作总数，用于测试是否结束
 
+// 随机生成新元素
 function randomNewOne() {
     let emptys = [];
     for (let i = 0; i < ROWSIZE; i++) {
@@ -26,24 +27,39 @@ function mergeLeft(rows) {
         let last;
         let curNow = 0;
         for (let j = 0; j < COLUMNSIZE; j++) {
+            if (rows[i][j] === 0) { continue; }
             if (last === rows[i][j]) {
                 last = 2 * last;
                 rows[i][curNow] = last;
                 rows[i][j] = 0;
-                curNow++;
                 operate++;
-            } else if (rows[i][j] !== 0 && curNow !== j) {
-                last = rows[i][j];
-                rows[i][curNow] = last;
-                rows[i][j] = 0;
-                curNow++;
-                operate++;
+            } else {
+                if (curNow === j) {
+                    last = rows[i][curNow];
+                } else if (rows[i][curNow] === 0) {
+                    last = rows[i][j];
+                    rows[i][curNow] = last;
+                    rows[i][j] = 0;
+                    operate++;
+                } else if (rows[i][curNow] !== 0) {
+                    last = rows[i][j];
+                    curNow++;
+                    if (curNow === j) {
+                        last = rows[i][curNow];
+                    } else {
+                        rows[i][j] = 0;
+                        rows[i][curNow] = last;
+                        operate++;
+                    }
+                }
             }
         }
     }
+    updateInterFace();
     if (operate !== 0) {
         randomNewOne();
     }
+    updateInterFace();
 }
 
 // 向右合并
@@ -53,83 +69,152 @@ function mergeRight(rows) {
         let last;
         let curNow = COLUMNSIZE - 1;
         for (let j = COLUMNSIZE - 1; j >= 0; j--) {
+            if (rows[i][j] === 0) { continue; }
             if (last === rows[i][j]) {
                 last = 2 * last;
                 rows[i][curNow] = last;
                 rows[i][j] = 0;
-                curNow--;
                 operate++;
-            } else if (rows[i][j] !== 0 && curNow !== j) {
-                last = rows[i][j];
-                rows[i][curNow] = last;
-                rows[i][j] = 0;
-                curNow--;
-                operate++;
+            } else {
+                if (curNow === j) {
+                    last = rows[i][curNow];
+                } else if (rows[i][curNow] === 0) {
+                    last = rows[i][j];
+                    rows[i][curNow] = last;
+                    rows[i][j] = 0;
+                    operate++;
+                } else if (rows[i][curNow] !== 0) {
+                    last = rows[i][j];
+                    curNow--;
+                    if (curNow === j) {
+                        last = rows[i][curNow];
+                    } else {
+                        rows[i][j] = 0;
+                        rows[i][curNow] = last;
+                        operate++;
+                    }
+                }
             }
         }
     }
+    updateInterFace();
     if (operate !== 0) {
         randomNewOne();
     }
+    updateInterFace();
 }
 
 // 向上合并
 function mergeTop(rows) {
     let operate = 0;
-    for (let j = 0; j < COLUMNSIZE; i++) {
+    for (let j = 0; j < COLUMNSIZE; j++) {
         let last;
         let curNow = 0;
         for (let i = 0; i < ROWSIZE; i++) {
+            if (rows[i][j] === 0) { continue; }
             if (last === rows[i][j]) {
                 last = 2 * last;
                 rows[curNow][j] = last;
                 rows[i][j] = 0;
-                curNow++;
                 operate++;
-            } else if (rows[i][j] !== 0 && curNow !== j) {
-                last = rows[i][j];
-                rows[curNow][j] = last;
-                rows[i][j] = 0;
-                curNow++;
-                operate++;
+            } else {
+                if (curNow === i) {
+                    last = rows[curNow][j];
+                } else if (rows[curNow][j] === 0) {
+                    last = rows[i][j];
+                    rows[curNow][j] = last;
+                    rows[i][j] = 0;
+                    operate++;
+                } else if (rows[curNow][j] !== 0) {
+                    last = rows[i][j];
+                    curNow++;
+                    if (curNow === i) {
+                        last = rows[curNow][j];
+                    } else {
+                        rows[i][j] = 0;
+                        rows[curNow][j] = last;
+                        operate++;
+                    }
+                }
             }
         }
     }
+    updateInterFace();
     if (operate !== 0) {
         randomNewOne();
     }
+    updateInterFace();
 }
 
 // 向下合并
 function mergeBottom(rows) {
     let operate = 0;
-    for (let j = 0; j < COLUMNSIZE; i++) {
+    for (let j = 0; j < COLUMNSIZE; j++) {
         let last;
         let curNow = ROWSIZE - 1;
         for (let i = ROWSIZE - 1; i >= 0; i--) {
+            if (rows[i][j] === 0) { continue; }
             if (last === rows[i][j]) {
                 last = 2 * last;
                 rows[curNow][j] = last;
                 rows[i][j] = 0;
-                curNow--;
                 operate++;
-            } else if (rows[i][j] !== 0 && curNow !== j) {
-                last = rows[i][j];
-                rows[curNow][j] = last;
-                rows[i][j] = 0;
-                curNow--;
-                operate++;
+            } else {
+                if (curNow === i) {
+                    last = rows[curNow][j];
+                } else if (rows[curNow][j] === 0) {
+                    last = rows[i][j];
+                    rows[curNow][j] = last;
+                    rows[i][j] = 0;
+                    operate++;
+                } else if (rows[curNow][j] !== 0) {
+                    last = rows[i][j];
+                    curNow--;
+                    if (curNow === i) {
+                        last = rows[curNow][j];
+                    } else {
+                        rows[i][j] = 0;
+                        rows[curNow][j] = last;
+                        operate++;
+                    }
+                }
             }
         }
     }
+    updateInterFace();
     if (operate !== 0) {
         randomNewOne();
     }
+    updateInterFace();
 }
 
-function updateInterFace() { }
+// 键盘点击事件
+function keyDown(e) {
+    if (e.keyCode === 38) {
+        mergeTop(rows);
+    } else if (e.keyCode === 40) {
+        mergeBottom(rows);
+    } else if (e.keyCode === 37) {
+        mergeLeft(rows);
+    } else if (e.keyCode === 39) {
+        mergeRight(rows);
+    }
+}
 
-function init() {
+function updateInterFace() {
+    let ul = $(".content");
+    for (let i = 0; i < ROWSIZE; i++) {
+        for (let j = 0; j < COLUMNSIZE; j++) {
+            if (rows[i][j] === 0) {
+                ul.children()[i].children[j].innerText = "";
+            } else {
+                ul.children()[i].children[j].innerText = rows[i][j];
+            }
+        }
+    }
+}
+
+function startGame() {
     rows =
         [[0, 0, 0, 0],
         [0, 0, 0, 0],
@@ -138,4 +223,8 @@ function init() {
 
     randomNewOne();
     randomNewOne();
+    updateInterFace();
+    document.addEventListener("keydown", keyDown);
 }
+
+document.querySelector(".start").addEventListener("click", startGame);
